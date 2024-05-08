9bba27e375a3bee2f2a34c98f3cb304e7bbdf8ea
David Cotter <daveguy28@gmail.com>
2024-05-08 09:21:24 +0000

    base and contact adding

--- run.py
+++ run.py
@@ -10,9 +10,18 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
-
