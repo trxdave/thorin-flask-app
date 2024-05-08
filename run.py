edab671fc0588a58bf7f7b52b454263bce15b0fe
David Cotter <daveguy28@gmail.com>
2024-05-08 13:58:50 +0000

    data to be set up

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About")
+    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
 
 
 @app.route("/contact")
